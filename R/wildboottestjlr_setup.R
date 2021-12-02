e7e9644f5bac45b8dbf3a38741cec9cb2d9d8665
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-12-02 19:11:33 +0100

    install WildBootTests.jl from Julia central rep; change install_julia argument to FALSE default

--- R/wildboottestjlr_setup.R
+++ R/wildboottestjlr_setup.R
@@ -1,7 +1,7 @@
-wildboottestjlr_setup <- function (install_julia = TRUE, install_wildboottests = TRUE, julia_path){
+wildboottestjlr_setup <- function (install_julia = FALSE, install_wildboottests = TRUE, julia_path){
 
   #' Connect R and Julia via JuliaConnectoR & installs Julia dependency packages (if required)
-  #' @param install_julia Logical. True by default. Should Julia be installed?
+  #' @param install_julia Logical. True by FALSE Should Julia be installed?
   #' @param install_wildboottests Logical. TRUE by default. Installs WildBootTests.jl
   #' @param julia_path Character. Path to Julia installation - required if WildBootTests.jl needs to be installed.
   #' @importFrom JuliaCall install_julia julia_install_package
@@ -14,7 +14,7 @@ wildboottestjlr_setup <- function (install_julia = TRUE, install_wildboottests =
   if(install_wildboottests){
     message("Note that setting up Julia & installing WildBootTests.jl will take some time - approx. 1-2 minutes.")
     JuliaCall::julia_setup(julia_path)
-    JuliaCall::julia_install_package("https://github.com/droodman/WildBootTests.jl")
+    JuliaCall::julia_install_package("WildBootTests.jl")
   }
 
   message("Please restart your R sessions.")
